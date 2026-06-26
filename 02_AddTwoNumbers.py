ca5068f1abc4d9f876a0e38eaabd241687d7aeda
Shivarajadharshini Durairajan <shivarajadharshini43@gmail.com>
2026-06-26 09:51:19 +0530

    Create 02_AddTwoNumbers.py
    Added program to add two numbers

---
+++ 02_AddTwoNumbers.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("Sum =", sum)
+
+# Sample Input:
+# 10
+# 20
+
+# Output:
+# Sum = 30
