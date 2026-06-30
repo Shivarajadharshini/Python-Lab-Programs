be796dccf96271ee2290d9670acd3ef102cc5758
Shivarajadharshini Durairajan <shivarajadharshini43@gmail.com>
2026-06-30 10:42:10 +0530

    Create Factorial.py
    Added factorial program using for loop

---
+++ Loops/Factorial.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial *= i
+
+print("Factorial of", num, "=", factorial)
+
+# Sample Input:
+# 5
+
+# Output:
+# Factorial of 5 = 120
