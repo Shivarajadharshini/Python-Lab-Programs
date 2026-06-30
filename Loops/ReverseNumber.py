59b8f6909c8df3b0055860c7e8268f3e4a625167
Shivarajadharshini Durairajan <shivarajadharshini43@gmail.com>
2026-06-30 10:45:33 +0530

    Create ReverseNumber.py
    Added reverse number program using while loop

---
+++ Loops/ReverseNumber.py
@@ -0,0 +1,16 @@
+num = int(input("Enter a number: "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print("Reversed Number =", reverse)
+
+# Sample Input:
+# 1234
+
+# Output:
+# Reversed Number = 4321
